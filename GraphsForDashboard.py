399415cb2f1f4cf9f74b14be169a0460e5d9278b
Vivek <vivekbhardwaj0067@gmail.com>
2020-05-15 13:50:06 +0530

    A working function that accept country queries and plots a line chart for cases and deaths per day added

--- GraphsForDashboard.py
+++ GraphsForDashboard.py
@@ -6,23 +6,37 @@
 # c=pd.read_csv(io.StringIO(s.decode('utf-8')))
 # c.head()
 
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#reading the csv
 df=pd.read_csv('covid data')
-countries=set(df['countriesAndTerritories'])
-plt.figure(figsize=(500,350),dpi=100)
-plt.title('Deaths due to covid 19')
-for country in countries :
-    temp=df.loc[df['countriesAndTerritories']==country]
-    plt.plot(temp['dateRep'],temp['deaths'],label=country)
-plt.legend()
-plt.xlabel('Date')
-plt.ylabel('Deaths')
-# plt.show()
-plt.savefig('Deaths plot',dpi=100)
+listOfCountries=list(set(df['countriesAndTerritories']))
+
+
+def plot(*args) :
+    plt.figure(figsize=(50,35),dpi=100)
+    plt.title('Deaths/Cases due to covid-19 on a daily basis')
+    for country in args[0] :
+        temp=df.loc[df['countriesAndTerritories']==country]
+        temp=temp[::-1]
+        plt.plot(temp['dateRep'],temp['deaths'],label='Deaths in '+country)
+        plt.plot(temp['dateRep'],temp['cases'],'--',label='Cases in '+country)
+
+    temp=df.loc[df['countriesAndTerritories']==args[0][0]]
+    date=temp['dateRep']
+    date=date[::13]
+    plt.xticks(date)
+    plt.xlabel('Date')
+    plt.ylabel('Deaths')
+    plt.legend()
+    plt.show()
+
+while(True) : 
+    countries = input('Enter the country name you want to see stastics : \n').split()
+    plot(countries)
+
 
 
 
